66544d9c19b0dd1e3a6cd41c2fea40a027566e76
Anand Simmy <anandsimmy7@gmail.com>
2020-05-23 19:14:23 +0530

    routing added for collection categories

--- src/pages/collection/collection-component.jsx
+++ src/pages/collection/collection-component.jsx
@@ -3,12 +3,18 @@ import { connect } from 'react-redux'
 
 import './collection-styles.scss'
 import { selectCollectionCatgeory } from '../../redux/shop/shop-selectors'
+import CollectionItem from '../../components/collection-item/collection-item-component'
 
 const CollectionPage= ({ collection }) => {
-    console.log(collection)
+    const { title, items }= collection
     return (
     <div className='collection-page'>
-        <h1>COLLECTION PAGE</h1>
+        <h1 className='title'>{title}</h1>
+        <div className='items'>
+            {
+                items.map(item => <CollectionItem key={item.id} item={item} />)
+            }
+        </div>
     </div>
 )}
 
