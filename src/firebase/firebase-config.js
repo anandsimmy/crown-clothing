ecdddeacbced6708fc531c12842ff3c8b15e3b16
Anand Simmy <anandsimmy7@gmail.com>
2020-05-27 15:52:10 +0530

    firebase config added

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBY-84owDZBYNheFRHTv9epCpVC89UWOvE",
+    authDomain: "crown-db-66a57.firebaseapp.com",
+    databaseURL: "https://crown-db-66a57.firebaseio.com",
+    projectId: "crown-db-66a57",
+    storageBucket: "crown-db-66a57.appspot.com",
+    messagingSenderId: "646898735149",
+    appId: "1:646898735149:web:f5d7ade92dca402fe700a4",
+    measurementId: "G-MJ7GHK0MH8"
+  }
+
+  export default firebaseConfig
