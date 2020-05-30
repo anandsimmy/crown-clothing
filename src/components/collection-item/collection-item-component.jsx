20ff41aae40a3fa901d20a392172e407202e8440
Anand Simmy <anandsimmy7@gmail.com>
2020-05-30 21:22:25 +0530

    styled components added to shop page

--- src/components/collection-item/collection-item-component.jsx
+++ src/components/collection-item/collection-item-component.jsx
@@ -3,22 +3,29 @@ import { connect } from 'react-redux'
 
 import './collection-item-styles.scss'
 
-import CustomButton from '../custom-button/custom-button-component'
+import {
+    CollectionItemContainer,
+    CollectionFooterContainer,
+    AddButton,
+    BackgroundImage,
+    NameContainer,
+    PriceContainer
+ } from './collection-item-styles'
 import { addItem } from '../../redux/cart/cart-actions'
 
 const CollectionItem=({ item, AddItem}) => {
     const { name, price, imageUrl }= item
     return (
-        <div className='collection-item'>
-            <div className='image' style={{
-                backgroundImage: `url(${imageUrl})`
-            }}/>
-            <div className='collection-footer'>
-                <span className='name'>{name}</span>
-                <span className='price'>{price}</span>
-            </div>
-            <CustomButton inverted onClick={()=>AddItem(item)}>Add to Cart</CustomButton>
-        </div>
+        <CollectionItemContainer>
+            <BackgroundImage className='image' imageUrl={imageUrl} />
+            <CollectionFooterContainer>
+            <NameContainer>{name}</NameContainer>
+            <PriceContainer>{price}</PriceContainer>
+            </CollectionFooterContainer>
+            <AddButton onClick={() => addItem(item)} inverted>
+                Add to cart
+            </AddButton>
+        </CollectionItemContainer>
     )
 }
 
--- src/components/custom-button/custom-button-component.jsx
+++ src/components/custom-button/custom-button-component.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 
-import './custom-button-styles.scss'
+import { CustomButtonContainer } from './custom-button-styles'
 
-//need to implement 'CustomButtonContainer' styled-components over here. 
-//But now, it breaks shop page buttons when added. Need to debug
-
-const CustomButton=({ children, isGoogleSignIn, inverted, ...props }) => {
+const CustomButton=({ children, ...props }) => {
     return ( 
-        <button className={`${ inverted ? 'inverted' : '' } ${ isGoogleSignIn ? 'google-sign-in' : '' } custom-button`} {...props}>
+        <CustomButtonContainer {...props}>
             {children}
-        </button>
+        </CustomButtonContainer>
     )
 }
 
