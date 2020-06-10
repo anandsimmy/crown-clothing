43412d0f9f68e13d52f4831d2814b948adb0328e
Anand Simmy <anandsimmy7@gmail.com>
2020-06-10 13:05:00 +0530

    shop component modified with collection containers

--- src/pages/shop/shop-component.jsx
+++ src/pages/shop/shop-component.jsx
@@ -1,17 +1,11 @@
 import React, { Component } from 'react'
 import { Route} from 'react-router-dom'
 import { connect } from 'react-redux'
-import { createStructuredSelector } from 'reselect'
 
-import CollectionOverview from '../../components/collection-overview/collection-overview-component';
-import CollectionPage from  '../collection-page/collection-page-component'
-import withSpinner from '../../components/with-spinner/with-spinner-component'
+import CollectionOverviewContainer from '../../components/collection-overview/collection-overview-container';
+import CollectionPageContainer from  '../collection-page/collection-page-container'
 
 import { fetchCollectionDataAsyncOperation } from '../../redux/shop/shop-actions'
-import { selectIsComponentDataLoaded } from '../../redux/shop/shop-selectors'
-
-const CollectionOverviewWithSpinner= withSpinner(CollectionOverview)
-const CollectionPageWithSpinner= withSpinner(CollectionPage)
 
 class ShopPage extends Component {
 
@@ -21,22 +15,19 @@ class ShopPage extends Component {
 
     render() {
 
-        const { match, isComponentDataLoaded }= this.props
+        const { match }= this.props
 
         return(
         <div className='shop-page'>
-            <Route exact path={`${match.path}`} render={(props) => <CollectionOverviewWithSpinner isLoading={!isComponentDataLoaded} {...props} />} />
-            <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={!isComponentDataLoaded} {...props} />} />
+            <Route exact path={`${match.path}`} component={CollectionOverviewContainer}/>
+            <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
         </div>)
     }
 }
 
-const mapStatetoProps= createStructuredSelector({
-    isComponentDataLoaded: selectIsComponentDataLoaded
-})
 
 const mapDispatchToProps=dispatch => ({
     fetchCollectionDataStart: () => dispatch(fetchCollectionDataAsyncOperation())
 })
 
-export default connect(mapStatetoProps, mapDispatchToProps)(ShopPage)
+export default connect(null, mapDispatchToProps)(ShopPage)
