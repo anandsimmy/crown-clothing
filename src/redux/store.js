1c8a60ba66c78f9927bf8125ed4b5271d8ad57f5
Anand Simmy <anandsimmy7@gmail.com>
2020-06-13 10:29:04 +0530

    integrated redux-saga with redux-store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,22 @@
 import { createStore, applyMiddleware } from 'redux'
 import { persistStore } from 'redux-persist'
 import logger from 'redux-logger'
-import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
 
 import rootReducer from './root-reducer'
+import { fetchCollectionStart } from '../redux/shop/shop-sagas'
 
-const middlewares= [thunk]
+const sagaMiddleware= createSagaMiddleware()
+
+const middlewares= [sagaMiddleware]
 
 if(process.env.NODE_ENV === 'development'){
     middlewares.push(logger)
 }
 
 const store= createStore(rootReducer, applyMiddleware(...middlewares))
+
+sagaMiddleware.run(fetchCollectionStart)
 
 const persistor= persistStore(store)
 
