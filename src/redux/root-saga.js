241dbb20c80522c4528369c5a195c16f684bc7af
Anand Simmy <anandsimmy7@gmail.com>
2020-06-14 17:55:21 +0530

    added root-saga

---
+++ src/redux/root-saga.js
@@ -0,0 +1,9 @@
+import { all, call } from 'redux-saga/effects'
+
+import  { fetchCollectionStart } from './shop/shop-sagas'
+
+export function* rootSaga(){
+    yield all([
+        call(fetchCollectionStart)
+    ])
+}
