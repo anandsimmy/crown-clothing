9e01ce2477fe2d0ffe2f4ce0eab8482e764ff5d6
Anand Simmy <anandsimmy7@gmail.com>
2020-06-16 09:12:19 +0530

    check user session saga added

--- src/redux/user/user-sagas.js
+++ src/redux/user/user-sagas.js
@@ -1,6 +1,6 @@
 import { takeLatest, put, call, all } from 'redux-saga/effects'
 
-import { auth, googleProvider, createUserProfileInDB } from '../../firebase/firebase-utils'
+import { auth, googleProvider, getCurrentUser, createUserProfileInDB } from '../../firebase/firebase-utils'
 
 import userActionTypes from './user-action-types'
 import { signInSuccess, signInFailure } from './user-actions'
@@ -18,10 +18,12 @@ export function* snapShotFromDB(userAuth){
     }
 }
 
-export function* emailSignIn({payload: {email, password}}){
+export function* checkUserSession(){
     try{
-        const { user }= yield auth.signInWithEmailAndPassword(email, password)
-        yield snapShotFromDB(user)
+        const userAuth= yield getCurrentUser()
+        if(!userAuth)
+            return
+        yield snapShotFromDB(userAuth)
     }catch(error){
         yield put(signInFailure(error))
     }
@@ -37,15 +39,28 @@ export function* googleSignIn(){
     }
 }
 
-export function* googleSignInStart(){
+export function* emailSignIn({payload: {email, password}}){
+    try{
+        const { user }= yield auth.signInWithEmailAndPassword(email, password)
+        yield snapShotFromDB(user)
+    }catch(error){
+        yield put(signInFailure(error))
+    }
+}
+
+export function* onCheckUserSession(){
+    yield takeLatest(userActionTypes.CHECK_USER_SESSION, checkUserSession)
+}
+
+export function* onGoogleSignInStart(){
     yield takeLatest(userActionTypes.GOOGLE_SIGN_IN_START, googleSignIn)
 }
 
-export function* emailSignInStart(){
+export function* onEmailSignInStart(){
     yield takeLatest(userActionTypes.EMAIL_SIGN_IN_START, emailSignIn)
 }
 
 export function* userSagas(){
-    yield all([call(googleSignInStart), call(emailSignInStart)])
+    yield all([call(onGoogleSignInStart), call(onEmailSignInStart), call(onCheckUserSession)])
 }
 
