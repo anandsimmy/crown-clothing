b201b814115ef50cd75cd87d7fe3d891541ffa2b
Anand Simmy <anandsimmy7@gmail.com>
2020-06-16 13:07:07 +0530

    removed sign-up logic from component

--- src/components/sign-up/sign-up-component.jsx
+++ src/components/sign-up/sign-up-component.jsx
@@ -1,10 +1,12 @@
 import React, { Component} from 'react'
+import { connect } from 'react-redux'
+
+import './sign-up-styles.scss'
 
 import FormInput from '../form-input/form-input-component'
 import CustomButton from '../custom-button/custom-button-component'
-import { auth, createUserProfileInDB } from '../../firebase/firebase-utils'
 
-import './sign-up-styles.scss'
+import { signUpStart } from '../../redux/user/user-actions'
 
 class SignUp extends Component {
     constructor(){
@@ -19,25 +21,22 @@ class SignUp extends Component {
 
     handleSubmit=async event => {
         event.preventDefault()
+
         const { displayName, email, password, confirmPassword }= this.state
+        const { signUpStart }= this.props
+
         if(password !== confirmPassword){
             alert('passwords donot match')
             return;
         }
-        try{
-            const { user }= await auth.createUserWithEmailAndPassword(email, password)
-            await createUserProfileInDB(user, { displayName })
-            this.setState({
-                displayName: '',
-                email: '',
-                password: '',
-                confirmPassword: ''
-            })
-        }
-        catch (error){
-            console.log(error)
-        }
-
+        console.log('sign-up', email, password)
+        signUpStart({ displayName, email, password })
+        this.setState({
+                    displayName: '',
+                    email: '',
+                    password: '',
+                    confirmPassword: ''
+                })
     }
 
     handleChange=(event) => {
@@ -48,7 +47,9 @@ class SignUp extends Component {
     }
 
     render(){
+
         const { displayName, email, password, confirmPassword }= this.state
+
         return(
             <div className='sign-up'>
                 <h2 className='title'>I do not have an Account</h2>
@@ -93,4 +94,8 @@ class SignUp extends Component {
     }
 }
 
-export default SignUp
+const mapDispatchToProps= dispatch => ({
+    signUpStart: userObj => dispatch(signUpStart(userObj))
+})
+
+export default connect(null, mapDispatchToProps)(SignUp)
