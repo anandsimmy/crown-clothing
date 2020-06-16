a58ae3fef600735c899287fc25d3e55464018327
Anand Simmy <anandsimmy7@gmail.com>
2020-06-16 09:11:53 +0530

    promise based check user session added

--- src/firebase/firebase-utils.js
+++ src/firebase/firebase-utils.js
@@ -64,6 +64,15 @@ export const transformCollectionArray=(snapShotObj)=>{
 export const auth= firebase.auth()
 export const firestore= firebase.firestore()
 
+export const getCurrentUser=() => {
+    return new Promise((resolve, reject)=>{
+        const unsubscribe= auth.onAuthStateChanged((userAuth)=>{
+            unsubscribe()
+            resolve(userAuth)
+        }, reject)
+    })
+}
+
 export const googleProvider= new firebase.auth.GoogleAuthProvider()
 googleProvider.setCustomParameters({ prompt: 'select_account' })
 export const signInWithGoogle= () => auth.signInWithPopup(googleProvider)
