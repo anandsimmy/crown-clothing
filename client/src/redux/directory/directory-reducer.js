860f4e03ffba224fb3911a3d16e9b21dfe4bb396
Anand Simmy <anandsimmy7@gmail.com>
2020-07-15 14:57:25 +0530

    modified homepage

--- client/src/redux/directory/directory-reducer.js
+++ client/src/redux/directory/directory-reducer.js
@@ -1,37 +1,37 @@
 const INITIAL_STATE=  {
     sections: [
     {
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-      id: 1,
-      linkUrl: 'shop/hats'
-    },
-    {
       title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      imageUrl: 'https://i.ibb.co/M6hHc3F/brown-trench.png',
       id: 2,
       linkUrl: 'shop/jackets'
     },
     {
       title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      imageUrl: 'https://i.ibb.co/QcvzydB/nikes-red.png',
       id: 3,
       linkUrl: 'shop/sneakers'
     },
     {
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-      size: 'large',
-      id: 4,
-      linkUrl: 'shop/womens'
+      title: 'hats',
+      imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
+      id: 1,
+      linkUrl: 'shop/hats'
     },
     {
       title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      imageUrl: 'https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png',
       size: 'large',
       id: 5,
       linkUrl: 'shop/mens'
-    }
+    },
+    {
+      title: 'womens',
+      imageUrl: 'https://i.ibb.co/KmSkMbH/striped-sweater.png',
+      size: 'large',
+      id: 4,
+      linkUrl: 'shop/womens'
+    }  
   ]
 }
 
