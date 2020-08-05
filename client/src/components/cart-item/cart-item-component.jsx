9ffa0de45d716e6f1efd6f1bf667c6bdf024bfeb
Anand Simmy <anandsimmy7@gmail.com>
2020-08-05 17:30:09 +0530

    memoized cart item component

--- client/src/components/cart-item/cart-item-component.jsx
+++ client/src/components/cart-item/cart-item-component.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { memo } from 'react'
 
 import './cart-item-styles.scss'
 
@@ -12,4 +12,4 @@ const CartItem=({ item: { name, price, imageUrl, quantity } }) => (
     </div>
 )
 
-export default CartItem
+export default memo(CartItem)
