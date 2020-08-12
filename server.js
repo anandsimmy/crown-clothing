a4886067568218c5b4f1dbcf6dc1bcf683b42851
Anand Simmy <anandsimmy7@gmail.com>
2020-08-12 13:13:23 +0530

    added compression library for gzipping

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express= require('express')
 const cors= require('cors')
 const bodyParser= require('body-parser')
 const path= require('path')
+const compression= require('compression')
 
 const app= express()
 const port= process.env.PORT || 5000
@@ -12,6 +13,7 @@ if(process.env.NODE_ENV !== 'production'){
 
 const stripe= require('stripe')(process.env.STRIPE_SECRET_KEY)
 
+app.use(compression())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded( { extended:true } ))
 app.use(cors())
