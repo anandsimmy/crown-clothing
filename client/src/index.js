a85cc7138991e8b7388ac866fc33c8a2565deaf6
Anand Simmy <anandsimmy7@gmail.com>
2020-08-25 11:00:41 +0530

    enabled service worker

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
+import * as serviceWorker from './serviceWorker'
 
 import './index.css'
 import storeObj from './redux/store'
@@ -20,3 +21,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register()
