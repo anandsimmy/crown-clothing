970c30f5e661df960d9e2ebfa3fd9a9490963d11
Anand Simmy <anandsimmy7@gmail.com>
2020-08-25 11:00:16 +0530

    enforced https and added service-worker file

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors= require('cors')
 const bodyParser= require('body-parser')
 const path= require('path')
 const compression= require('compression')
+const enforce= require('express-sslify')
 
 const app= express()
 const port= process.env.PORT || 5000
@@ -17,6 +18,7 @@ app.use(compression())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded( { extended:true } ))
 app.use(cors())
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, 'client/build')))
@@ -25,6 +27,10 @@ if(process.env.NODE_ENV === 'production'){
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
     })
 }
+
+app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.resolve('__dirname', '..', 'build', 'service-worker.js'))
+})
 
 app.post('/payment', (req, res) => {
     const body= {
