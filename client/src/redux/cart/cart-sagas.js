0fad881d96c5d52c4117b91385ab40c136fe8299
Anand Simmy <anandsimmy7@gmail.com>
2020-08-28 15:51:50 +0530

    added addtocart listener saga

--- client/src/redux/cart/cart-sagas.js
+++ client/src/redux/cart/cart-sagas.js
@@ -1,10 +1,26 @@
-import { all, call, takeLatest, put } from 'redux-saga/effects'
+import { all, call, takeLatest, takeEvery, put } from 'redux-saga/effects'
 
+import { addItemsToOnlineUserCart } from '../../firebase/firebase-utils'
 import userActionTypes from '../user/user-action-types'
-import { clearCart } from './cart-actions'
+import cartActionTypes from './cart-action-types'
+import { clearCart, addItem } from './cart-actions'
 
 export function* clearShopCart(){
     yield put(clearCart())
+}
+
+export function* addItemToAllCarts({ payload }){
+    try {
+        yield addItemsToOnlineUserCart(payload)
+        yield put(addItem(payload))
+    }
+    catch{
+        console.log('error when adding item')
+    }
+}
+
+export function* onAddItemToAllCarts(){
+    yield takeEvery(cartActionTypes.ADD_ITEM_TO_ALL_CARTS, addItemToAllCarts)
 }
 
 export function* onClearCart(){
@@ -13,6 +29,7 @@ export function* onClearCart(){
 
 export function* cartSagas(){
     yield all([
-        call(onClearCart)
+        call(onClearCart),
+        call(onAddItemToAllCarts)
     ])
 }
