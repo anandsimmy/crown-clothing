fe31f24d57980e2b5057201d7d4fad464fd60239
Anand Simmy <anandsimmy7@gmail.com>
2020-08-29 14:37:31 +0530

    added onlinecart feature

--- client/src/components/checkout-item/checkout-item-component.jsx
+++ client/src/components/checkout-item/checkout-item-component.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import './checkout-item-styles.scss'
-import { clearItem, addItem, decrementItemQuantity } from '../../redux/cart/cart-actions'
+import { addItemInAllCarts, subtractItemInAllCarts, clearItemInAllCarts } from '../../redux/cart/cart-actions'
 
-const CheckoutItem= ({ cartItem, clearItem, addItemQuantity, decrementItemQuantity })  => {
+const CheckoutItem= ({ cartItem, add, subtract, clear })  => {
     const { name, quantity, price, imageUrl } = cartItem
     return (
     <div className='checkout-item'>
@@ -13,19 +13,19 @@ const CheckoutItem= ({ cartItem, clearItem, addItemQuantity, decrementItemQuanti
         </div>
     <span className='name'>{name}</span>
     <span className='quantity'>
-        <div className='arrow' onClick={()=>decrementItemQuantity(cartItem)}>&#10094;</div>
+        <div className='arrow' onClick={()=>subtract(cartItem)}>&#10094;</div>
             <span className='value'>{quantity}</span>
-        <div className='arrow' onClick={()=>addItemQuantity(cartItem)}>&#10095;</div>
+        <div className='arrow' onClick={()=>add(cartItem)}>&#10095;</div>
     </span>
     <span className='price'>{price}</span>
-    <div className='remove-button' onClick={()=>clearItem(cartItem)}>&#10005;</div>
+    <div className='remove-button' onClick={()=>clear(cartItem)}>&#10005;</div>
     </div>
 )}
 
 const mapDispatchToProps= dispatch => ({
-    clearItem: item => dispatch(clearItem(item)),
-    addItemQuantity: item => dispatch(addItem(item)),
-    decrementItemQuantity: item => dispatch(decrementItemQuantity(item))
+    add: item => dispatch(addItemInAllCarts(item)),
+    subtract: item => dispatch(subtractItemInAllCarts(item)),
+    clear: item => dispatch(clearItemInAllCarts(item))
 })
 
 export default connect(null, mapDispatchToProps)(CheckoutItem)
